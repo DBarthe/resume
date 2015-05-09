2f46cbd6dd98910b0ff8e5e4e32268f3211a7226
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-05-09 13:16:41 +0200

    First model TechnicalSkill

--- website/models.py
+++ website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class TechnicalSkill(models.Model):
+  LANGUAGE = 'LA'
+  SYSTEM = 'SY'
+  DATABASE = 'DA'
+  SOFTWARE = 'SO'
+  OTHER = 'OT'
+  CATEGORY_CHOICES = (
+    (LANGUAGE, 'Language'),
+    (SYSTEM, 'System'),
+    (DATABASE, 'Database'),
+    (SOFTWARE, 'Software'),
+    (OTHER, 'Other')
+  )
+  category = models.CharField(
+    max_length=2, choices=CATEGORY_CHOICES, default=OTHER)
+  name = models.CharField(max_length=255) # no need to translate
+  date = models.DateField(auto_now_add=True)
+  weight = models.IntegerField(default=0,
+    help_text='Will determine the display order, higher first')
+  icon = models.FileField(null=True)
+
+  def __unicode__(self):
+    return self.default_name
+
