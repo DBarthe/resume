07564074a34aaf546052897dfe6d8b6c250812f2
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-05-10 21:44:51 +0200

    customize admin

--- website/admin.py
+++ website/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
+from nested_admin import NestedAdmin, NestedStackedInline
 
-from .models import TechnicalSkill, Skill, SkillTranslate
+from .models import TechnicalSkill, Skill, SkillTranslation, Experience, ExperienceTranslation, ExperienceTask
 
-class SkillTranslateInline(admin.StackedInline):
-  model = SkillTranslate
+class SkillTranslationInline(admin.StackedInline):
+  model = SkillTranslation
   min_num = 2
   extra = 0
+  inline_classes = ('grp-collapse grp-open',)
+  classes = ('grp-collapse grp-open',)
+
 
 class SkillAdmin(admin.ModelAdmin):
-  inlines = [SkillTranslateInline]
+  inlines = [SkillTranslationInline]
+
+class ExperienceTaskInline(NestedStackedInline):
+  model = ExperienceTask
+  extra = 0
+  min_num = 1
+
+class ExperienceTranslationInline(NestedStackedInline):
+  model = ExperienceTranslation
+  inlines =  [ExperienceTaskInline]
+  extra = 0
+  min_num = 2
+  inline_classes = ('grp-collapse grp-open',)
+  classes = ('grp-collapse grp-open',)
+
+class ExperienceAdmin(NestedAdmin):
+  inlines = [ExperienceTranslationInline]
 
 admin.site.register(TechnicalSkill)
 admin.site.register(Skill, SkillAdmin)
+admin.site.register(Experience, ExperienceAdmin)
